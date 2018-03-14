3a8f771e859ca92fc89eab6fb4a2cf49f1444805
Andrey Dunaev <andrey361361@gmail.com>
2018-03-14 07:11:23 +0500

    9 commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u63768_yii',
+    'username' => 'u63768',
+    'password' => 'tcqakWPCeprjm',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
