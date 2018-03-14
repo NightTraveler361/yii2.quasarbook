ef8918c020566d9f9f42a87625ef23b0e8267192
Andrey Dunaev <andrey361361@gmail.com>
2018-03-14 06:24:53 +0500

    6 commit

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=u63768_yii',
     'username' => 'u63768',
-    'password' => 'rmkqdCzrjxavpc8',
+    'password' => 'tcqakWPCeprjm3q',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
